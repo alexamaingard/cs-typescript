2ffd7f1e5547f0822c9d61831613a67aa3365cd6
alexamaingard <maingard.alexa@gmail.com>
2022-03-24 17:56:15 +0100

    fix all type bugs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,104 +1,95 @@
-import { SetStateAction, useState } from 'react'
+import { SetStateAction, useState } from "react";
 
-import { CompletedTodoList } from './CompletedTodoList'
-import { TodoList } from './TodoList'
-import { TodoType } from './Todo'
+import { CompletedTodoList } from "./CompletedTodoList";
+import { TodoList } from "./TodoList";
+import { TodoType } from "./Todo";
 
-import '../styles/styles.css'
+import "../styles/styles.css";
 
-// Define this as an array of TodoType
-const initialTodos = []
+const initialTodos: Array<TodoType> = [];
 
 export const App = () => {
-  // Initialise this with a correctly defined initialTodos
-  const [todos, setTodos] = useState(undefined);
-  
-  const [todoInput, setTodoInput] = useState('')
-  const [showCompletedTodos, setShowCompletedTodos] = useState(true)
+    const [todos, setTodos] = useState(initialTodos);
 
-  //Declare the type of the parameter
-  //Declare the type this function returns, too 
-  const addTodo = text => {
-    if (todos.some(todo => todo.text.toLowerCase() === text.toLowerCase())) {
-      alert('That todo already exists!')
-      return
-    }
-    setTodos([...todos, { text, completed: false }])
-  }
+    const [todoInput, setTodoInput] = useState("");
+    const [showCompletedTodos, setShowCompletedTodos] = useState(true);
 
-  //Declare the type of the parameter
-  //Declare the type this function returns, too 
-  const handleChange = e => setTodoInput(e.target.value)
+    const addTodo = (text: string): void => {
+        if(todos.some((todo) => todo.text.toLowerCase() === text.toLowerCase())){
+            alert("That todo already exists!");
 
-  //Declare the type of the parameter
-  //Declare the type this function returns, too 
-  const handleSubmit = e => {
-    e.preventDefault()
-    addTodo(todoInput)
-    setTodoInput('')
-  }
+            return;
+        }
 
-  //Declare the type of the parameter
-  //Declare the type this function returns, too 
-  const toggleTodoCompletion = target => {
-    const updatedTodos = todos.map(todo =>
-      todo === target ? { ...todo, completed: !todo.completed } : todo
-    )
-    setTodos(updatedTodos)
-  }
+        setTodos([...todos, { text, completed: false }]);
+    };
 
-  //Declare the type of the parameter
-  //Declare the type this function returns, too 
-  const removeTodo = target => {
-    setTodos(todos.filter(todo => todo !== target))
-  }
+    const handleChange = (e: {target: { value: SetStateAction<string> }}): void => setTodoInput(e.target.value);
 
-  const incompleteTodos = todos.filter(todo => !todo.completed)
-  const completedTodos = todos.filter(todo => todo.completed)
+    const handleSubmit = (e: { preventDefault: () => void }): void => {
+        e.preventDefault();
+        addTodo(todoInput);
+        setTodoInput("");
+    };
 
-  return (
-    <div className="App">
-      <main>
-        <section>
-          <h2 className="title">OPTIONS</h2>
-          <label>
-            Show completed
-            <input
-              className="show-completed"
-              onChange={e => setShowCompletedTodos(e.target.checked)}
-              type="checkbox"
-              checked={showCompletedTodos}
-            />
-          </label>
-        </section>
-        <section>
-          <h2 className="title">ADD ITEM</h2>
-          <form className="add-item" onSubmit={handleSubmit}>
-            <input
-              className="text-input"
-              type="text"
-              name="text"
-              required
-              minlength="3"
-              onChange={handleChange}
-              value={todoInput}
-            />
-            <button type="submit">Add</button>
-          </form>
-        </section>
-        <TodoList
-          incompleteTodos={incompleteTodos}
-          toggleTodoCompletion={toggleTodoCompletion}
-          removeTodo={removeTodo}
-        />
-        {showCompletedTodos ? (
-          <CompletedTodoList
-            completedTodos={completedTodos}
+    const toggleTodoCompletion = (target: TodoType): void => {
+        const updatedTodos = todos.map((todo) =>
+            todo === target ? { ...todo, completed: !todo.completed } : todo
+        );
+
+        setTodos(updatedTodos);
+    };
+
+    const removeTodo = (target: TodoType): void => {
+        setTodos(todos.filter((todo) => todo !== target));
+    };
+
+    const incompleteTodos = todos.filter((todo) => !todo.completed);
+    const completedTodos = todos.filter((todo) => todo.completed);
+
+    return (
+        <div className="App">
+        <main>
+            <section>
+            <h2 className="title">OPTIONS</h2>
+            <label>
+                Show completed
+                <input
+                className="show-completed"
+                onChange={(e) => setShowCompletedTodos(e.target.checked)}
+                type="checkbox"
+                checked={showCompletedTodos}
+                />
+            </label>
+            </section>
+            <section>
+            <h2 className="title">ADD ITEM</h2>
+            <form className="add-item" onSubmit={handleSubmit}>
+                <input
+                className="text-input"
+                type="text"
+                name="text"
+                required
+                minLength={3}
+                onChange={handleChange}
+                value={todoInput}
+                />
+                <button type="submit">Add</button>
+            </form>
+            </section>
+            <TodoList
+            incompleteTodos={incompleteTodos}
             toggleTodoCompletion={toggleTodoCompletion}
             removeTodo={removeTodo}
-          />
-        ) : null}
-      </main>
-    </div>
-  )
-}
+            />
+            {showCompletedTodos ? (
+                <CompletedTodoList
+                    completedTodos={completedTodos}
+                    toggleTodoCompletion={toggleTodoCompletion}
+                    removeTodo={removeTodo}
+                />
+            ) : null}
+        </main>
+        </div>
+    );
+};
